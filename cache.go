c57939c470a6f4fb04ed58824ca1fcf46e5cbfbc
kim (grufwub) <grufwub@gmail.com>
2020-04-15 12:43:02 +0100

    custom max cache size + max file in cache size, improve cache.go readability
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- cache.go
+++ cache.go
@@ -7,6 +7,10 @@ import (
     "container/list"
 )
 
+const (
+    BytesInMegaByte = 1048576.0
+)
+
 var (
     FileMonitorSleepTime = time.Duration(*CacheCheckFreq) * time.Second
 
@@ -77,7 +81,7 @@ func checkCacheFreshness(cache *FileCache) {
         file.Unlock()
     }
 
-    /* Done! We can release regular cache read lock */
+    /* Done! We can release cache read lock */
     cache.CacheMutex.RUnlock()
 }
 
@@ -124,20 +128,21 @@ func (fc *FileCache) Init(size int, newFileFunc func(path string) File) {
 }
 
 func (fc *FileCache) Fetch(path string) ([]byte, *GophorError) {
-    /* Get read lock, try get file and defer read unlock */
+    /* Get cache map read lock then check if file in cache map */
     fc.CacheMutex.RLock()
     fileElement, ok := fc.CacheMap[path]
 
     if ok {
-        /* File in cache -- before doing anything get read lock */
+        /* File in cache -- before doing anything get file read lock */
         fileElement.File.RLock()
 
-        /* Now check is fresh */
+        /* Check file is marked as fresh */
         if !fileElement.File.IsFresh() {
-            /* File not fresh! Swap read for write-lock */
+            /* File not fresh! Swap file read for write-lock */
             fileElement.File.RUnlock()
             fileElement.File.Lock()
 
+            /* Reload file contents from disk */
             gophorErr := fileElement.File.LoadContents()
             if gophorErr != nil {
                 /* Error loading contents, unlock all mutex then return error */
@@ -146,25 +151,47 @@ func (fc *FileCache) Fetch(path string) ([]byte, *GophorError) {
                 return nil, gophorErr
             }
 
-            /* Updated! Swap back to file read lock for upcoming content read */
+            /* Updated! Swap back file write for read lock */
             fileElement.File.Unlock()
             fileElement.File.RLock()
         }
     } else {
-        /* File not in cache -- Swap cache map read lock for write lock */
-        fc.CacheMutex.RUnlock()
-        fc.CacheMutex.Lock()
+        /* Before we do ANYTHING, we need to check file-size on disk */
+        stat, err := os.Stat(path)
+        if err != nil {
+            return nil, &GophorError{ FileStatErr, err }
+        }
 
-        /* New file init function */
+        /* Use supplied new file function */
         file := fc.NewFile(path)
 
-        /* NOTE: file isn't in cache yet so no need to lock file mutex */
+        /* NOTE: file isn't in cache yet so no need to lock file write mutex
+         * before loading from disk
+         */
         gophorErr := file.LoadContents()
         if gophorErr != nil {
             /* Error loading contents, unlock all mutex then return error */
             fc.CacheMutex.Unlock()
             return nil, gophorErr
         }
+
+        /* Compare file size (in MB) to CacheFileSizeMax, if larger just get file
+         * contents, unlock all mutex and don't bother caching. 
+         */
+        if float64(stat.Size()) / BytesInMegaByte > *CacheFileSizeMax {
+            logSystem("File too big for cache, skipping: %s\n", path)
+            b := file.Contents()
+            fc.CacheMutex.RUnlock()
+            return b, nil
+        }
+
+        /* File not in cache -- Swap cache map read for write lock.
+         * NOTE: Here we don't need a list mutex lock as it is impossible
+         *       for any other goroutine to get this lock while we have a cache
+         *       _write_ lock. Due to the way this Fetch() function is written
+         */
+        fc.CacheMutex.RUnlock()
+        fc.CacheMutex.Lock()
 
         /* Place path in FileList to get back element */
         element := fc.FileList.PushFront(path)
@@ -173,44 +200,44 @@ func (fc *FileCache) Fetch(path string) ([]byte, *GophorError) {
         fileElement = &FileElement{ file, element }
         fc.CacheMap[path] = fileElement
 
-        /* If we're at capacity, remove last item in list from map+list */
+        /* If we're at capacity, remove last item in list from cachemap + cache list */
         if fc.FileList.Len() == fc.Size {
             removeElement := fc.FileList.Back()
 
-            /* Have to perform type assertion, if error we'll exit */
+            /* Have to perform type assertion even if we know value will always be string.
+             * If not, we may as well os.Exit(1) out since error is fatal
+             */
             removePath, ok := removeElement.Value.(string)
             if !ok {
-                logSystemFatal("Non-string found in cache list!\n")
+                logSystemFatal("non-string found in cache list!\n")
             }
-
-            /* Get lock to ensure no-one else using */
-            fc.CacheMap[removePath].File.Lock()
-            fc.CacheMap[removePath].File.Unlock()
 
             /* Now delete. We don't need ListMutex lock as we have cache map write lock */
             delete(fc.CacheMap, removePath)
             fc.FileList.Remove(removeElement)
         }
 
+        /* Before unlocking cache mutex, lock file read for upcoming call to .Contents() */
+        file.RLock()
+
         /* Swap cache lock back to read */
         fc.CacheMutex.Unlock()
         fc.CacheMutex.RLock()
-
-        /* Get file read lock for upcoming content read */
-        file.RLock()
     }
 
-    /* Read file contents into new variable for return, then unlock all */
+    /* Get list lock, ready to update placement in list */
+    fc.ListMutex.Lock()
+
+    /* Read file contents into new variable for return, then unlock file read lock */
     b := fileElement.File.Contents()
     fileElement.File.RUnlock()
 
-    /* First get list lock, now update placement in list */
-    fc.ListMutex.Lock()
+    /* Update placement in list then unlock */
     fc.FileList.MoveToFront(fileElement.Element)
     fc.ListMutex.Unlock()
 
+    /* Finally we can unlock the cache map read lock, we are done :) */
     fc.CacheMutex.RUnlock()
 
     return b, nil
 }
-
--- flags.go
+++ flags.go
@@ -6,21 +6,21 @@ import (
 
 var (
     /* Base server settings */
-    ServerRoot     = flag.String("root", "/var/gopher", "Change server root directory.")
-    ServerHostname = flag.String("hostname", "127.0.0.1", "Change server hostname (FQDN).")
-    ServerPort     = flag.Int("port", 70, "Change server port (0 to disable unencrypted traffic).")
+    ServerRoot       = flag.String("root", "/var/gopher", "Change server root directory.")
+    ServerHostname   = flag.String("hostname", "127.0.0.1", "Change server hostname (FQDN).")
+    ServerPort       = flag.Int("port", 70, "Change server port (0 to disable unencrypted traffic).")
 //    ServerTlsPort  = flag.Int("tls-port", 0, "Change server TLS/SSL port (0 to disable).")
 //    ServerTlsCert  = flag.String("cert", "", "Change server TLS/SSL cert file.")
-    ExecAsUid      = flag.Int("uid", 1000, "Change UID to drop executable privileges to.")
-    ExecAsGid      = flag.Int("gid", 100, "Change GID to drop executable privileges to.")
+    ExecAsUid        = flag.Int("uid", 1000, "Change UID to drop executable privileges to.")
+    ExecAsGid        = flag.Int("gid", 100, "Change GID to drop executable privileges to.")
 
     /* Logging settings */
-    SystemLog      = flag.String("system-log", "", "Change server system log file (blank outputs to stderr).")
-    AccessLog      = flag.String("access-log", "", "Change server access log file (blank outputs to stderr).")
-    LoggingType    = flag.Int("log-type", 0, "Change server log file handling -- 0:default 1:disable")
+    SystemLog        = flag.String("system-log", "", "Change server system log file (blank outputs to stderr).")
+    AccessLog        = flag.String("access-log", "", "Change server access log file (blank outputs to stderr).")
+    LoggingType      = flag.Int("log-type", 0, "Change server log file handling -- 0:default 1:disable")
 
     /* Cache settings */
-    CacheCheckFreq = flag.Float64("cache-check", 30, "Change file cache freshness check frequency (in seconds).")
-    CacheSize      = flag.Int("cache-size", 1000, "Change individual file cache size, measured in file count.")
-    CacheFileSzMax = flag.Float64("cache-file-max", 5, "Change maximum file size to be cached (in megabytes).")
+    CacheCheckFreq   = flag.Float64("cache-check", 30, "Change file cache freshness check frequency (in seconds).")
+    CacheSize        = flag.Int("cache-size", 1000, "Change individual file cache size, measured in file count.")
+    CacheFileSizeMax = flag.Float64("cache-file-max", 5, "Change maximum file size to be cached (in megabytes).")
 )
