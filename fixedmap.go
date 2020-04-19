5756b46cdcb6a9a6f2213fa87d47585c848228d5
kim (grufwub) <grufwub@gmail.com>
2020-04-19 20:57:56 +0100

    add code comments to fixed map
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

--- fixedmap.go
+++ fixedmap.go
@@ -4,12 +4,21 @@ import (
     "container/list"
 )
 
+/* FixedMap:
+ * A fixed size map that pushes the last
+ * used value from the stack if size limit
+ * is reached and user attempts .Put()
+ */
 type FixedMap struct {
     Map  map[string]*MapElement
     List *list.List
     Size int
 }
 
+/* MapElement:
+ * Simple structure to wrap pointer to list
+ * element and stored map value together.
+ */
 type MapElement struct {
     Element *list.Element
     Value   *File
@@ -36,8 +45,13 @@ func (fm *FixedMap) Put(key string, value *File) {
     fm.Map[key] = &MapElement{ element, value }
 
     if fm.List.Len() == fm.Size {
+        /* We're at capacity! SIR! */
         element = fm.List.Back()
+
+        /* We don't check here as we know this is ALWAYS a string */
         key, _ := element.Value.(string)
+
+        /* Finally delete the map entry and list element! */
         delete(fm.Map, key)
         fm.List.Remove(element)
     }
@@ -46,9 +60,11 @@ func (fm *FixedMap) Put(key string, value *File) {
 func (fm *FixedMap) Remove(key string) {
     elem, ok := fm.Map[key]
     if !ok {
+        /* We don't have this key, return */
         return
     }
 
+    /* Remove the selected element */
     delete(fm.Map, key)
     fm.List.Remove(elem.Element)
 }
