12660a8a4672eda248a2b1af43bf265ba8f928cd
kim (grufwub) <grufwub@gmail.com>
2020-04-20 15:36:33 +0100

    replace build script with multi-build script
    Signed-off-by: kim (grufwub) <grufwub@gmail.com>

---
+++ build-all.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+PROJECT='gophor'
+OUTDIR='build'
+
+build() {
+    echo "Building $PROJECT for $1_$2..."
+    env GOOS="$1" GOARCH="$2" go build -trimpath -o="$OUTDIR/$PROJECT.$1.$2" -buildmode="$3"
+    echo ''
+}
+
+mkdir -p "$OUTDIR"
+build 'linux' '386'   'pie'
+build 'linux' 'amd64' 'pie'
+build 'linux' 'arm'   'pie'
+build 'linux' 'arm64' 'pie'
+build 'linux' 'mips'  'default'
--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-go build -trimpath -buildmode=pie
